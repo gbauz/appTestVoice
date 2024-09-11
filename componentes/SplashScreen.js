a6ee84a67138d3669f31f665957d348dd21a5bd6
Giovanni Bauz <92232987+gbauz@users.noreply.github.com>
2024-09-11 16:22:13 -0500

    splash actualizado

--- componentes/SplashScreen.js
+++ componentes/SplashScreen.js
@@ -3,13 +3,10 @@ import { Text, StyleSheet, View, Image } from 'react-native';
 
 import Menu from './Menu';
 
-
-
 export default function CustomText() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-   
     const timer = setTimeout(() => {
       setIsLoading(false);
     }, 5000);
@@ -20,11 +17,19 @@ export default function CustomText() {
   if (isLoading) {
     return (
       <View style={styles.splashScreen}>
+        <View style={styles.imageContainer}>
         <Image
-          source={require('../imagenes/logodigital.jpeg')}
-          style={styles.logo}
-        />
-        <Text style={styles.splashText}>Cargando...</Text>
+            source={require('../imagenes/logoapp.png')}
+            style={styles.logoapp}
+          />
+          <Text style={styles.textoPrincipal}>Speak Profile</Text>
+          <Text style={styles.overlayText}>Desarrollado por</Text>
+          <Image
+            source={require('../imagenes/logodigital.jpeg')}
+            style={styles.logo}
+          />
+        </View>
+      
       </View>
     );
   }
@@ -39,16 +44,40 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     flex: 1,
-    backgroundColor: '#fff', // Puedes cambiar el color de fondo si lo deseas
+    backgroundColor: '#fff',
+  },
+  imageContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  
+  },
+  logo: {
+    width: 250,
+    height: 250,
+    resizeMode: 'contain',
+    marginTop: 2,  
+  },
+  logoapp:{
+    width: 550,
+    height: 500,
+    resizeMode: 'contain',
+   
+  },
+  textoPrincipal:{
+    marginBottom:40,
+    color: 'black',
+    fontSize: 55,
+  },
+  overlayText: {
+    marginBottom: -85, 
+    color: 'black',
+    fontSize: 25,
+    fontWeight:'bold',
   },
   splashText: {
     fontSize: 24,
     color: 'black',
     marginTop: 20,
-  },
-  logo: {
-    width: 350,
-    height: 400,
-    resizeMode: 'contain',
   },
 });
